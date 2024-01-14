f0a4072e8dac4cfd15d794b5531547d5fd89823f
Altin Nimani <altin.nimani.dev@gmail.com>
2024-01-14 15:49:20 +0100

    add employee model

---
+++ Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManagementSystem.Models
+{
+    [Table("employees")]
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Column("first_name")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        [Column("last_name")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        [Column("position")]
+        public string Position { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Range(0, 10000000, ErrorMessage = "Salary must be between 0 and 10,000,000")]
+        [Column("salary")]
+        public decimal Salary { get; set; }
+    }
+}
