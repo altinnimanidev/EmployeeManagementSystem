fc61ecb73b6ed7c487065182d218634d4da3096e
Altin Nimani <altin.nimani.dev@gmail.com>
2024-01-14 21:15:03 +0100

    add employees edit

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
-﻿using EmployeeManagementSystem.Services.EmployeeServices;
+﻿using EmployeeManagementSystem.Models;
+using EmployeeManagementSystem.Services.EmployeeServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManagementSystem.Controllers
@@ -12,6 +13,61 @@ namespace EmployeeManagementSystem.Controllers
         public EmployeesController(IEmployeeService employeeService)
         {
             _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id == 0)
+            {
+                // Handle invalid ID (e.g., return a not found view or redirect)
+                return NotFound();
+            }
+
+            var employee = await _employeeService.GetEmployeeByIdAsync(id);
+            if (employee == null)
+            {
+                // Handle the case where the employee doesn't exist
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        public async Task<IActionResult> Edit(int id, [FromForm] Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Handle the case where the model is not valid
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                Employee? newEmployee = new Employee();
+                if (employee.Id == 0)
+                {
+                    newEmployee = await _employeeService.AddEmployeeAsync(employee);
+                }
+                else
+                {
+                    newEmployee = await _employeeService.UpdateEmployeeAsync(employee);
+                }
+
+                if (newEmployee == null)
+                {
+                    return NotFound();
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                // Log the exception and handle the error
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error updating employee");
+            }
         }
 
         [HttpGet]
